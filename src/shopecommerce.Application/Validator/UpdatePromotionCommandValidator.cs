8a7ab4a72a79c7eb81170095cbc316729e0dbbf2
Đoàn Vọng Hiếu <112066867+VongHieu@users.noreply.github.com>
2023-10-05 10:43:07 +0700

    update

--- src/shopecommerce.Application/Validator/UpdatePromotionCommandValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using FluentValidation;
-using shopecommerce.Application.Commands.PromotionCommand.UpdatePromotion;
-
-namespace shopecommerce.Application.Validator
-{
-    public class UpdatePromotionCommandValidator : AbstractValidator<UpdatePromotionCommand>
-    {
-    }
-}
