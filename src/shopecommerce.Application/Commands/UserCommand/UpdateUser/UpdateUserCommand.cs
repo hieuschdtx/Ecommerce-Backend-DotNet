496e5655d6dc9f2a51a522c5f169c176a95823c5
Đoàn Vọng Hiếu <112066867+VongHieu@users.noreply.github.com>
2023-11-29 08:57:46 +0700

    update user

--- src/shopecommerce.Application/Commands/UserCommand/UpdateUser/UpdateUserCommand.cs
+++ src/shopecommerce.Application/Commands/UserCommand/UpdateUser/UpdateUserCommand.cs
@@ -1,4 +1,5 @@
-﻿using shopecommerce.Domain.Commons.Commands;
+﻿using Microsoft.AspNetCore.Http;
+using shopecommerce.Domain.Commons.Commands;
 using shopecommerce.Domain.Models;
 
 namespace shopecommerce.Application.Commands.UserCommand.UpdateUser
@@ -7,7 +8,8 @@ namespace shopecommerce.Application.Commands.UserCommand.UpdateUser
     {
         public string? full_name { get; set; }
         public string? address { get; set; }
-        public string? avatar { get; set; }
+        public string? email { get; set; }
+        public IFormFile? avatar_file { get; set; }
         public DateOnly? dob { get; set; }
         public bool? gender { get; set; }
     }
